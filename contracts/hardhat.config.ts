073a7cdc655768c3ba3d081fe3527a366cb052f8
Ray Deck <ray@raydeck.com>
2022-07-17 14:50:34 -0400

    feat: enable hardhat to run without env vars (useful for local dev)

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,29 +2,32 @@ import { HardhatUserConfig } from "hardhat/config";
 import dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
 dotenv.config();
-
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    polygonMumbai: {
-      url: process.env.ALCHEMY_MUMBAI,
-      accounts: [process.env.PK || ""],
-    },
-    polygon:{
-      url: process.env.ALCHEMY_POLYGON,
-      accounts: [process.env.PK || ""],
-    }
+    polygonMumbai: process.env.ALCHEMY_MUMBAI
+      ? {
+          url: process.env.ALCHEMY_MUMBAI,
+          accounts: [process.env.PK || ""],
+        }
+      : undefined,
+    polygon: process.env.ALCHEMY_POLYGON
+      ? {
+          url: process.env.ALCHEMY_POLYGON,
+          accounts: [process.env.PK || ""],
+        }
+      : undefined,
   },
   etherscan: {
     apiKey: {
       polygonMumbai: process.env.POLYGON_API || "",
       polygon: process.env.POLYGON_API || "",
     },
-  }
-
+  },
 };
-
+if (!config.networks!.polygonMumbai) delete config.networks!.polygonMumbai;
+if (!config.networks!.polygon) delete config.networks!.polygon;
 export default config;
